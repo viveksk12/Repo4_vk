f92b849b1d4bf1de3cf7ce5ac471970abc7d2007
LENOVO <LENOVO@DESKTOP-GVFKA4N>
2023-12-10 19:44:32 +0530

    ArithmeticOperations assignment

---
+++ src/ClassAssignmentPKG/ArithmeticOperations.java
@@ -0,0 +1,31 @@
+package ClassAssignmentPKG;
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double average = (double) (num1 + num2) / 2;
+        int distance = Math.abs(difference);
+        int maximum = Math.max(num1, num2);
+        int minimum = Math.min(num1, num2);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Average: " + average);
+        System.out.println("Distance: " + distance);
+        System.out.println("Maximum: " + maximum);
+        System.out.println("Minimum: " + minimum);
+    }
+}
